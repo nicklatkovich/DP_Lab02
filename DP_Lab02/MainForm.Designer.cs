417317abe9a7b9c581a9b75db071d34c121282dd
NickLatkovich <nicklatkovich@gmail.com>
2017-09-19 19:54:42 +0300

    Done

--- DP_Lab02/MainForm.Designer.cs
+++ DP_Lab02/MainForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace DP_Lab02 {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,12 +23,89 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent( ) {
-            this.components = new System.ComponentModel.Container( );
+            this.Button_OpenFile = new System.Windows.Forms.Button();
+            this.TextBox_InputText = new System.Windows.Forms.TextBox();
+            this.ListBox_Entropy = new System.Windows.Forms.ListBox();
+            this.Label_Entropy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button_OpenFile
+            // 
+            this.Button_OpenFile.AutoSize = true;
+            this.Button_OpenFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.Button_OpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_OpenFile.ForeColor = System.Drawing.Color.Silver;
+            this.Button_OpenFile.Location = new System.Drawing.Point(12, 12);
+            this.Button_OpenFile.Name = "Button_OpenFile";
+            this.Button_OpenFile.Size = new System.Drawing.Size(163, 35);
+            this.Button_OpenFile.TabIndex = 0;
+            this.Button_OpenFile.Text = "Open text file";
+            this.Button_OpenFile.UseVisualStyleBackColor = false;
+            this.Button_OpenFile.Click += new System.EventHandler(this.Button_OpenFile_Click);
+            // 
+            // TextBox_InputText
+            // 
+            this.TextBox_InputText.AllowDrop = true;
+            this.TextBox_InputText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.TextBox_InputText.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBox_InputText.ForeColor = System.Drawing.Color.Silver;
+            this.TextBox_InputText.Location = new System.Drawing.Point(12, 53);
+            this.TextBox_InputText.Multiline = true;
+            this.TextBox_InputText.Name = "TextBox_InputText";
+            this.TextBox_InputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox_InputText.Size = new System.Drawing.Size(339, 324);
+            this.TextBox_InputText.TabIndex = 1;
+            this.TextBox_InputText.TextChanged += new System.EventHandler(this.TextBox_InputText_TextChanged);
+            // 
+            // ListBox_Entropy
+            // 
+            this.ListBox_Entropy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ListBox_Entropy.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListBox_Entropy.ForeColor = System.Drawing.Color.Silver;
+            this.ListBox_Entropy.FormattingEnabled = true;
+            this.ListBox_Entropy.ItemHeight = 19;
+            this.ListBox_Entropy.Location = new System.Drawing.Point(357, 12);
+            this.ListBox_Entropy.Name = "ListBox_Entropy";
+            this.ListBox_Entropy.ScrollAlwaysVisible = true;
+            this.ListBox_Entropy.Size = new System.Drawing.Size(255, 365);
+            this.ListBox_Entropy.TabIndex = 2;
+            // 
+            // Label_Entropy
+            // 
+            this.Label_Entropy.AutoSize = true;
+            this.Label_Entropy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Entropy.ForeColor = System.Drawing.Color.Silver;
+            this.Label_Entropy.Location = new System.Drawing.Point(131, 396);
+            this.Label_Entropy.Name = "Label_Entropy";
+            this.Label_Entropy.Size = new System.Drawing.Size(136, 25);
+            this.Label_Entropy.TabIndex = 3;
+            this.Label_Entropy.Text = "Entropy = {0}";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.Label_Entropy);
+            this.Controls.Add(this.ListBox_Entropy);
+            this.Controls.Add(this.TextBox_InputText);
+            this.Controls.Add(this.Button_OpenFile);
+            this.Name = "MainForm";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DP_Lab02 by Vermilliest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Button_OpenFile;
+        private System.Windows.Forms.TextBox TextBox_InputText;
+        private System.Windows.Forms.ListBox ListBox_Entropy;
+        private System.Windows.Forms.Label Label_Entropy;
     }
 }
 
--- DP_Lab02/MainForm.cs
+++ DP_Lab02/MainForm.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace DP_Lab02 {
-    public partial class Form1 : Form {
-        public Form1( ) {
+    public partial class MainForm : Form {
+        public MainForm( ) {
             InitializeComponent( );
+        }
+
+        private void Button_OpenFile_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                try {
+                    TextBox_InputText.Text = File.ReadAllText(ofd.FileName);
+                } catch {
+                    // TODO: show error message
+                }
+            }
+        }
+
+        private void TextBox_InputText_TextChanged(object sender, EventArgs e) {
+            ListBox_Entropy.Items.Clear( );
+            Dictionary<char, ulong> charDictionary = new Dictionary<char, ulong>( );
+            ulong charsNumber = 0ul;
+            foreach (char c in TextBox_InputText.Text) {
+                if (char.IsLetter(c)) {
+                    char cToUp = char.ToUpper(c);
+                    if (charDictionary.ContainsKey(cToUp)) {
+                        charDictionary[cToUp]++;
+                    } else {
+                        charDictionary[cToUp] = 1ul;
+                    }
+                    charsNumber++;
+                }
+            }
+            double entropy = 0d;
+            foreach (KeyValuePair<char, ulong> pair in charDictionary) {
+                double probability = (double)pair.Value / charsNumber;
+                ListBox_Entropy.Items.Add(pair.Key + " - " + (probability * 100d).ToString(".0000") + "%");
+                entropy += probability * Math.Log(probability, 2);
+            }
+            entropy = -entropy;
+            Label_Entropy.Text = "Entropy = " + entropy.ToString( );
         }
     }
 }
